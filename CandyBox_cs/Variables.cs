3626a23fd205fccf9252c177d2c2ba16decc33d1
DiamondPRO02 <diamond.coder.backup@gmail.com>
2024-03-08 15:56:58 +0100

    -

--- CandyBox_cs/Variables.cs
+++ CandyBox_cs/Variables.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 
-public class Class1
+public interface IVariables
 {
-	public Class1()
-	{
-	}
+    public static bool debugMode;
+    public static int candies;
+    public static bool[] unlockedMenus = [
+        false, // 1. BoughtManuBar -> It get's some style and reveals that you will unlock more later
+        false, // 2. Config
+        false, // 3. Save? 
+        false, // 4. HealthBar
+        false, // 6. Inventory
+        false, // 5. Map
+        false, // 7. LollipopFarm
+        false // 8. Cauldron
+    ];
 }
