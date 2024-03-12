5115a9bd0bb4621d5404bbdfca2320dd4b2f60f6
DiamondCoder02 <diamond.coder.backup@gmail.com>
2024-03-12 11:54:24 +0100

    CandyBox has own repository

--- MainController.cs
+++ MainController.cs
@@ -1,5 +1,7 @@
-﻿using Uni_CsharpBasic_2024.CandyBox_cs;
-
+﻿using GenericsExampleBasic;
+using ListManagement;
+using Uni_CsharpBasic_2024.Lesson2;
+    
 namespace Uni_CsharpBasic_2024
 {
     internal class MainController
@@ -8,18 +10,28 @@ namespace Uni_CsharpBasic_2024
         {
             // https://stackoverflow.com/questions/69016697/create-a-standalone-exe-file-in-visual-studio-2019
             Console.WriteLine("Please choosse a number to start that program: ");
-            Console.WriteLine("0. CandyBox remade in console and C#");
             Console.WriteLine("1. Lesson1   HelloWorld");
             Console.WriteLine("2. Lesson2_1 Ask 3 Number");
             Console.WriteLine("3. Lesson2_2 Random Number Gen. (Task2.cs)");
             Console.WriteLine("4. Lesson4 TeacherShit1");
             Console.WriteLine("5. Lesson5 TeacherShit2");
-            // string? readline = Console.ReadLine();
-            string readline = "0"; // TODO, Fuck me
+            string? readline = Console.ReadLine();
             switch (readline)
             {
-                case "0":
-                    CB_init.CB_initiate();
+                case "1":
+                    test.Test.Lesson1();
+                    break;
+                case "2":
+                    Lesson2.Lesson2.Lesson2_1();
+                    break;
+                case "3":
+                    Task2.Lesson2_2();
+                    break;
+                case "4":
+                    Lesson4Thing.ProgStart(); 
+                    break;
+                case "5":
+                    Lesson5.ProgStart();
                     break;
                 case "69":
                     Console.WriteLine("https://f95zone.to/");
